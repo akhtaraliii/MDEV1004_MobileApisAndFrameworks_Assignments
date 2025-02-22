5789810cda6b9a5c5f652cc91653923d9ae26257
harshdeepsingh844 <harshdeep.mehta16@gmail.com>
2025-02-21 20:32:01 -0500

    add authentication routes, so, that user can register, login and logout

---
+++ Assignment2/ExpressApi/src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+/**
+ * Student: Akhtar Ali
+ * StudentID: 200568437
+ * Student: Harshdeep Singh
+ * StudentID: 200612779
+ * Date: 30th Jan 2025
+ */
+
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
+router.post('/logout', authController.logoutUser);
+
+module.exports = router;
