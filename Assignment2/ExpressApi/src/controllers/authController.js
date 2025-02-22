e2a9a75eafb6079ece33bb15d6edbff33459419f
harshdeepsingh844 <harshdeep.mehta16@gmail.com>
2025-02-21 21:33:38 -0500

    add user login functionality

--- Assignment2/ExpressApi/src/controllers/authController.js
+++ Assignment2/ExpressApi/src/controllers/authController.js
@@ -54,3 +54,43 @@ exports.registerUser = async(req,res)=>{
         });
     }
 }
+
+exports.loginUser = async(req,res)=>{
+    const{email,password}=req.body;
+
+    try{
+        //Validate
+        if(!email || !password){
+            return res.status(401).json({message:'All fields are required'});
+        }
+
+        //Check for existing user
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message:'Invalid email or password'});
+        }
+
+        //Compare password with hashed password
+        const isPasswordValid = await user.isValidPassword(password);
+        if(!isPasswordValid){
+            return res.status(401).json({message:'Invalid email or password'});
+        }
+
+        // Add user to logged in users
+        loggedInUsers.set(user.email, user);
+
+        //Successful login
+        res.status(200).json({
+            message:'Login successful',
+            userId: user._id,
+            email: user.email
+        });       
+    }
+    catch(error){
+        console.error('Login error details:', error);
+        res.status(500).json({
+            message:'Error logging in user',
+            error: error.message
+        });
+    }
+}
