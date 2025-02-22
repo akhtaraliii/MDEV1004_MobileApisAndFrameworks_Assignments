e92fce12ded9445d7d209c335d6e52922ecf4072
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-02-21 21:27:54 -0500

    Added the  user serialization and deserialization method

--- Assignment2/ExpressApi/src/config/passport.js
+++ Assignment2/ExpressApi/src/config/passport.js
@@ -23,4 +23,17 @@ passport.use(new LocalStrategy({
     }
 }));
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
+
 module.exports = passport;
