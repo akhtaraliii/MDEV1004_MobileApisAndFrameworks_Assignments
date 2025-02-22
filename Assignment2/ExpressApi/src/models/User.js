b6a637d55a762fad1aca64cbf0c1d830ead48a01
harshdeepsingh844 <harshdeep.mehta16@gmail.com>
2025-02-21 21:20:16 -0500

    add password encryption before saving the user

--- Assignment2/ExpressApi/src/models/User.js
+++ Assignment2/ExpressApi/src/models/User.js
@@ -6,3 +6,16 @@ const UserSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true}
 })
+
+// Encrypt the password before saving the user
+UserSchema.pre('save', async function(next) {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) return next();
+    
+    // Generate a salt with a factor of 10
+    const salt = await bcrypt.genSalt(10);
+    
+    // Hash the password using the generated salt
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
