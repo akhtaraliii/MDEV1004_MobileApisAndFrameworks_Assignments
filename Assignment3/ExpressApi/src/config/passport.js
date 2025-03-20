dfbc4234cd4efa1577ad912db55db83937500e7f
harshdeepsingh844 <harshdeep.mehta16@gmail.com>
2025-03-20 17:09:45 -0400

    Add basic Passport local strategy

--- Assignment3/ExpressApi/src/config/passport.js
+++ Assignment3/ExpressApi/src/config/passport.js
@@ -17,32 +17,11 @@ passport.use(new LocalStrategy({
 }, async (username, password, done) => {
     try {
         const user = await User.findOne({ username });
-        if (!user) {
-            return done(null, false, { message: 'Invalid username or password' });
+        if (!user || !(await user.isValidPassword(password))) {
+            return done(null, false, { message: 'Invalid credentials' });
         }
-
-        const isMatch = await user.comparePassword(password);
-        if (!isMatch) {
-            return done(null, false, { message: 'Invalid username or password' });
-        }
-
         return done(null, user);
     } catch (error) {
         return done(error);
     }
 }));
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error);
-    }
-});
-
-module.exports = passport;
