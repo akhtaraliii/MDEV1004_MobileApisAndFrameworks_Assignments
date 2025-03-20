f73fbc9df3fe109c33a786b8e16f8e3226b6f441
harshdeepsingh844 <harshdeep.mehta16@gmail.com>
2025-03-20 16:59:48 -0400

    add basic recipe controller with get operation

--- Assignment3/ExpressApi/src/controllers/recipeController.js
+++ Assignment3/ExpressApi/src/controllers/recipeController.js
@@ -11,7 +11,6 @@
 const Recipe = require('../models/Recipe');
 const { updateRecipesFile } = require('../utils/jsonUtils');
 
-// Get all recipes
 exports.getAllRecipes = async (req, res) => {
     try {
         const recipes = await Recipe.find();
@@ -23,117 +22,28 @@ exports.getAllRecipes = async (req, res) => {
     } catch (error) {
         res.status(500).json({
             status: 'error',
-            message: 'Error retrieving recipes',
-            error: error.message
+            message: 'Error retrieving recipes'
         });
     }
 };
 
-// Get recipe by ID
 exports.getRecipeById = async (req, res) => {
-  try {
-      const recipe = await Recipe.findById(req.params.id);
-      if (!recipe) {
-          return res.status(404).json({
-              status: 'fail',
-              message: 'Recipe not found'
-          });
-      }
-      res.status(200).json({
-          status: 'success',
-          data: recipe
-      });
-  } catch (error) {
-      res.status(500).json({
-          status: 'error',
-          message: 'Error retrieving recipe',
-          error: error.message
-      });
-  }
-};
-
-// Create new recipe
-exports.createRecipe = async (req, res) => {
     try {
-        const newRecipe = await Recipe.create(req.body);
-        
-        // Update JSON file with all recipes
-        await updateRecipesFile();
-
-        res.status(201).json({
-            status: 'success',
-            message: 'Recipe added successfully',
-            data: newRecipe
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 'error',
-            message: 'Error creating recipe',
-            error: error.message
-        });
-    }
-};
-
-// Update recipe
-exports.updateRecipe = async (req, res) => {
-    try {
-        const recipe = await Recipe.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {
-                new: true,
-                runValidators: true
-            }
-        );
-        
+        const recipe = await Recipe.findById(req.params.id);
         if (!recipe) {
             return res.status(404).json({
                 status: 'fail',
                 message: 'Recipe not found'
             });
         }
-
-        // Update JSON file with all recipes
-        await updateRecipesFile();
-
         res.status(200).json({
             status: 'success',
-            message: 'Recipe updated successfully',
             data: recipe
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: 'error',
-            message: 'Error updating recipe',
-            error: error.message
-        });
-    }
-};
-
-// Delete recipe
-exports.deleteRecipe = async (req, res) => {
-    try {
-        const recipe = await Recipe.findByIdAndDelete(req.params.id);
-        
-        if (!recipe) {
-            return res.status(404).json({
-                status: 'fail',
-                message: 'Recipe not found'
-            });
-        }
-
-        // Update JSON file with all recipes
-        await updateRecipesFile();
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Recipe deleted successfully'
         });
     } catch (error) {
         res.status(500).json({
             status: 'error',
-            message: 'Error deleting recipe',
-            error: error.message
+            message: 'Error retrieving recipe'
         });
     }
 };
