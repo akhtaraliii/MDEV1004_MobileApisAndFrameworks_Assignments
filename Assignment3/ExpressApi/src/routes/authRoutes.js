84a26a34c12f36e8cb952bda418752bd9504bb4b
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-03-20 17:34:47 -0400

    Added routes for the authentication

--- Assignment3/ExpressApi/src/routes/authRoutes.js
+++ Assignment3/ExpressApi/src/routes/authRoutes.js
@@ -11,9 +11,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.post('/logout', authController.logoutUser);
+router.get('/dashboard', auth, authController.dashboard);
 
 module.exports = router;
