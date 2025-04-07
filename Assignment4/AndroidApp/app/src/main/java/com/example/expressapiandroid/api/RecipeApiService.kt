aac4b1cfec8b6760c252e824b2b6affcf3da825c
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-04-06 23:06:01 -0400

    created recipe adapter that will help to fecth the data from database

--- Assignment4/AndroidApp/app/src/main/java/com/example/expressapiandroid/api/RecipeApiService.kt
+++ Assignment4/AndroidApp/app/src/main/java/com/example/expressapiandroid/api/RecipeApiService.kt
@@ -1,10 +1,20 @@
 package com.example.expressapiandroid.api
 
-import com.example.expressapiandroid.Recipe
+import com.example.expressapiandroid.models.Recipe
+import com.example.expressapiandroid.models.RecipeResponse
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface RecipeApiService {
     @GET("recipes")
     fun getRecipes(): Call<RecipeResponse>
+
+    @POST("recipes")
+    fun createRecipe(@Body recipe: Recipe): Call<RecipeResponse>
+
+    @PUT("recipes/{id}")
+    fun updateRecipe(@Path("id") id: String, @Body recipe: Recipe): Call<RecipeResponse>
+
+    @DELETE("recipes/{id}")
+    fun deleteRecipe(@Path("id") id: String): Call<RecipeResponse>
 }
