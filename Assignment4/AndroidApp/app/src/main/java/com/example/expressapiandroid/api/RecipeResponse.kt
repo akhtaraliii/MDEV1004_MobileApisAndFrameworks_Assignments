1cc0ca7efe501f87de2ffe2fdaf18074f1f71b26
Akhtar Ali <ali.akhtar1271@gmail.com>
2025-04-06 22:43:35 -0400

    Removed folders to make the changes again

--- Assignment4/AndroidApp/app/src/main/java/com/example/expressapiandroid/api/RecipeResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.expressapiandroid.api
-
-import com.example.expressapiandroid.Recipe
-
-data class RecipeResponse(
-    val status: String,
-    val results: Int,
-    val data: List<Recipe>
-)
